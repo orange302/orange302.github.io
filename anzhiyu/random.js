7778bb3358fa8961d3d5bb61495ef21adb0cd246
orange302 <3025340982@qq.com>
2023-07-03 12:04:51 +0800

    Site updated: 2023-07-03 12:04:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/3.html","posts/16107.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/16107.html","posts/3.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
